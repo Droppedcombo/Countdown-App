6e805396141ea08221c82bc2f49961ae72359bff
benrojas <droppedcombo7@gmail.com>
2018-04-12 11:09:39 -0600

    added mixins, and adjusted font for container message

--- src/components/clock.js
+++ src/components/clock.js
@@ -111,7 +111,12 @@ import { clearInterval } from 'timers';
                 <div className="countdown">
                 {
                     this.state.timeRemaining == 0 ?
-                        <h1> Happy Birthday! </h1>
+                      <div className="message-container">
+                          <p className="message-container__title">Countdown Complete</p>
+                          <p className="message-container__message">Happy Birthday</p>
+
+                       </div>
+        
                         :   
                         <div>
                           <ul className="countdown__clock">
